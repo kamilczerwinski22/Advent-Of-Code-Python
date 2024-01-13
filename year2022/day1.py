013c75eeea5742ea32c95a4fc52961de6f9420dd
Kamil Czerwinski <kamilcze31@gmail.com>
2024-01-13 20:28:36 +0100

    AoC 2022 python day 1 part 2

--- year2022/day1.py
+++ year2022/day1.py
@@ -1,16 +1,30 @@
+from typing import List, Any
+
 from InputReader import read_fixed_input
 from itertools import groupby
+from heapq import nlargest
+
+
+def get_elfs_cargo(inp: list) -> list:
+    return [sum(int(snack) for snack in cargo) for k, cargo in groupby(inp, key=lambda x: x != '') if k]
 
 
 def part_one(inp: list) -> int:
-    elfs = [sum(int(snack) for snack in cargo) for k, cargo in groupby(inp, key=lambda x: x != '') if k]
+    elfs = get_elfs_cargo(inp)
     return max(elfs)
+
+
+def part_two(inp: list) -> int:
+    elfs = get_elfs_cargo(inp)
+    return sum(nlargest(3, elfs))
 
 
 def main() -> None:
     inp = read_fixed_input()
     result_part_one = part_one(inp)
+    result_part_two = part_two(inp)
     print(f"Result for part one: {result_part_one}")
+    print(f"Result for part two: {result_part_two}")
 
 
 if __name__ == '__main__':
